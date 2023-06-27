8324cc619b8cc46651d00da363081cd3c2c540e0
Josh Burd <Jburdt@gmail.com>
2023-06-27 17:11:56 -0400

    styling

--- client/src/components/NewCourseForm.js
+++ client/src/components/NewCourseForm.js
@@ -44,64 +44,71 @@ const NewCourseForm = ({ addCourse }) => {
   };
 
   return (
-    <div id="new-course-div">
-    <Form onSubmit={handleSubmit}>
-      <h1>
-        Add New <Badge bg="primary">Course</Badge>
-      </h1>
-      <Form.Group className="mb-3" controlId="Name">
-        <div class="col-sm-4">
-        <Form.Label>Course Name</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+    <div
+      class="container d-flex justify-content-center"
+      id="new-course-div"
+    >
+      <Form onSubmit={handleSubmit} class="col-12">
+        <h1>
+          Add New <Badge bg="primary">Course</Badge>
+        </h1>
+        <Form.Group className="mb-3" controlId="Name">
+          <div class="col-sm-7">
+            <Form.Label>Course Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicAddress">
+          <div class="col-sm-7">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="Cost">
+          <div class="col-sm-7">
+            <Form.Label>Cost</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Cost"
+              value={cost}
+              onChange={(e) => setCost(e.target.value)}
+            />
+          </div>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="Image">
+          <div class="col-sm-7">
+            <Form.Label>Image</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Image"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+            />
+          </div>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        <div id="error-div" style={{ margin: "3px" }}>
+          {errors.map((error, index) => {
+            return (
+              <li style={{ color: "red" }} key={index}>
+                {error}!
+              </li>
+            );
+          })}
         </div>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicAddress">
-        <div class="col-sm-4">
-        <Form.Label>Address</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Address"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-        </div>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Cost">
-      <div class="col-sm-4">
-        <Form.Label>Cost</Form.Label>
-        <Form.Control
-          type="number"
-          placeholder="Cost"
-          value={cost}
-          onChange={(e) => setCost(e.target.value)}
-        />
-       </div> 
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Image">
-      <div class="col-sm-4">
-        <Form.Label>Image</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Image"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-        />
-       </div> 
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Add Course
-      </Button>
-      <div>
-        {errors.map((error, index) => {
-          return <li key={index}>{error}!</li>;
-        })}
-      </div>
-    </Form>
+      </Form>
     </div>
   );
 };
